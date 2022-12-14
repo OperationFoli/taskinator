988afc7d73cc173717b13af0893afb52d77c16e5
OperationFoli <64374998+OperationFoli@users.noreply.github.com>
2022-12-14 09:57:53 -0500

    added feature to 'add list item' to body list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-var formE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input['task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
+    // creates list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // creates div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // give it class name
+    listItemEl.className = "task-info";
+   
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";    
+    listItemEl.appendChild(taskInfoEl);
+    
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
   };
 
-formE1.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
